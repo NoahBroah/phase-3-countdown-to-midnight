98b76296f4b855fbaf62c9c99103526810e37925
Noah King <109630641+NoahBroah@users.noreply.github.com>
2022-10-01 17:33:01 -0400

    commit tree 788a90cd5ad562e3b3e07ef1c3c9439162b8bead

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown int
+    while int >= 1 do
+        puts "#{int} SECOND(S)!"
+        int -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+countdown 3
+
+def countdown_with_sleep int
+    while int >= 1 do
+        sleep 1
+        puts "#{int} SECOND(S)!"
+        int -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
